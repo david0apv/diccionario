9ac4bc56ec6ee23c1451208d9d13f75648c66c45
SportIn2018 <TiendaSportIn@gmail.com>
2018-05-28 21:48:14 -0500

    Primer commit ejercicio del diccionario

---
+++ e.php
@@ -0,0 +1,15 @@
+<?php
+switch ($_GET['term']) {
+	case 'Eavesdrop':
+		?>
+		<div class="entry">
+			<h3 class="term">EAVESDROP</h3>
+			<div class="part">verb.</div>
+				<div class="definition">
+					to listen secretly to a private conversation.
+				</div>
+			</div>
+		</div>
+		<?php
+		break;
+}
